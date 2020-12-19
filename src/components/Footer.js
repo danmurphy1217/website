0946e9431702587410431bf674ae5a7ef0c2bd96
dan murphy <danielmurph8@gmail.com>
2020-12-19 08:31:54 -0500

    convert navbar item actions to dynamically built array

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,9 @@
-import TwitterIcon from '@material-ui/icons/Twitter';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import AlternateEmailIcon from '@material-ui/icons/AlternateEmail';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import { makeStyles } from '@material-ui/core/styles';
-import { useState } from 'react';
 
 const useStyles = makeStyles({
     bottomNav: {
@@ -15,16 +13,19 @@ const useStyles = makeStyles({
 
 const NavBarItems = {
     GitHub: {
+        id: 1,
         value: "github",
         icon: <GitHubIcon/>,
         href: "https://github.com/danmurphy1217"
     },
     LinkedIn: {
+        id: 2,
         value: "linkedin",
         icon: <LinkedInIcon/>,
         href: "https://www.linkedin.com/in/daniel-p-murphy/"
     },
     Email: {
+        id: 3,
         value: "email",
         icon: <AlternateEmailIcon/>,
         href: "mailto:danielmurph8@gmail.com"
@@ -37,15 +38,13 @@ const Footer = () => {
     const BottomNavActions = []
     NavBarLabels.forEach( (label) => {
         BottomNavActions.push(
-            <BottomNavigationAction href={label.value} target="_blank" label={label} value={label.value} icon={label.icon}/>
+            <BottomNavigationAction key={NavBarItems[label].id} href={NavBarItems[label].href} target="_blank" label={label} value={NavBarItems[label].value} icon={NavBarItems[label].icon}/>
         )
     })
 
     return (
         <BottomNavigation showLabels className= {classes.bottomNav}>
-            <BottomNavigationAction href="https://github.com/danmurphy1217" target="_blank" label="GitHub" value="github" icon={<GitHubIcon/>}/>
-            <BottomNavigationAction href="https://www.linkedin.com/in/daniel-p-murphy/" target="_blank" label="LinkedIn" value="linkedin" icon={<LinkedInIcon/>}/>
-            <BottomNavigationAction href="mailto:danielmurph8@gmail.com" target="_blank" label="Email" value="email" icon={<AlternateEmailIcon/>}/>
+            {BottomNavActions}
         </BottomNavigation>
     )
 }
