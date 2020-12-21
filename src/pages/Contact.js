9060efee2b310bb6559ed3a2ecce7dd92e41bdeb
Dan Murphy <danielmurph8@gmail.com>
2020-12-21 08:16:16 -0500

    add divider to all pages, update plaintext to all caps

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,11 +2,14 @@ import Header from "../components/Header";
 import Container from "../styles/Global"
 import Footer from "../components/Footer"
 import { Descriptors } from "../components/HomeMain";
+import Divider from "../components/extras/Divider"
+
 const ContactPage = () => {
     return (
       <Container>
         <Header />
-        <Descriptors plaintext="Get in Touch"/>
+        <Descriptors plaintext="GET IN TOUCH"/>
+        <Divider />
         <Footer />
       </Container>
     );
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import ProjectSidebar from "../components/ProjectSidebar";
 import { Descriptors } from "../components/HomeMain";
+import Divider from "../components/extras/Divider"
 import Footer from "../components/Footer"
 import Container from "../styles/Global"
 
@@ -8,7 +9,8 @@ const ProjectsPage = () => {
     return (
         <Container>
             <Header />
-            <Descriptors plaintext="Projects" />
+            <Descriptors plaintext="PROJECTS" />
+            <Divider />
             <ProjectSidebar />
             <Footer/>
         </Container>
