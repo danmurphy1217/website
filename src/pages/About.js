2c15909f4c9a6d12f61d75c3850350e6186562ff
dan murphy <danielmurph8@gmail.com>
2020-12-26 19:07:07 -0500

    remove extra contents from the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,7 @@ import {Header} from "../components/Header";
 import {PageSupportingContent} from "../components/PageSupportingContent";
 import AboutPageContent from "./contents/AboutPageContents"
 import { Descriptors } from "../components/HomeMain";
-import AboutMain from "../components/AboutMain";
+// import AboutMain from "../components/AboutMain";
 import Footer from "../components/Footer";
 import Divider from "../components/extras/Divider";
 import Container from "../styles/Global";
@@ -15,7 +15,7 @@ const AboutPage = () => {
       <Divider />
       <Divider color="#C8C8C8"/>
       <PageSupportingContent content={AboutPageContent()} />
-      <AboutMain />
+      {/* <AboutMain /> */}
       <Footer />
     </Container>
   );
