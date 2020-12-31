beabdc8e59b7c28d000066f14ada0808fd252a18
dan murphy <danielmurph8@gmail.com>
2020-12-30 21:32:54 -0500

    a tag unit test for BookFixtureTwo

--- src/tests/Books.test.js
+++ src/tests/Books.test.js
@@ -59,3 +59,11 @@ it("should have a book description equal to the Description specified in the fir
     BookFixturesOne[0].Description
   );
 });
+
+it("should have two a tags for each book specified in the fixture and the first href should be BookFixturesTwo[0].Href", () => {
+    act(() => {
+      render(<Books books={BookFixturesTwo} />, container);
+    });
+    expect(container.querySelectorAll("a").length).toBe(BookFixturesTwo.length * 2);
+    expect(container.querySelectorAll("a")[0].href).toBe(BookFixturesTwo[0].Href);
+  });
