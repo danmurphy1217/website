3769bfcd874a4fdba354619e47a4f381e76eb4e5
Dan Murphy <danielmurph8@gmail.com>
2021-01-04 10:06:43 -0500

    V1: added icons for skills to the About Me page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import {Header} from "../components/Header";
 import {PageSupportingContent} from "../components/PageSupportingContent";
 import AboutPageContent from "./contents/AboutPageContents"
+import AboutMeIcons from "./contents/AboutMeIcons"
 import { Descriptors } from "../components/HomeMain";
 import Footer from "../components/Footer";
 import Divider from "../components/extras/Divider";
@@ -13,6 +14,7 @@ const AboutPage = () => {
       <Descriptors plaintext="ABOUT ME" />
       <Divider />
       <PageSupportingContent content={AboutPageContent()} />
+      <PageSupportingContent content={AboutMeIcons()} />
       <Footer />
     </Container>
   );
