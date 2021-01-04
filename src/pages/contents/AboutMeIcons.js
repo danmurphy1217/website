71f24cf3c061f2db85e550f4417ed0cda99a2bbf
Dan Murphy <danielmurph8@gmail.com>
2021-01-04 15:57:10 -0500

    icon styling and tooltips done, need to wrap them next

--- src/pages/contents/AboutMeIcons.js
+++ src/pages/contents/AboutMeIcons.js
@@ -1,13 +1,56 @@
+import React, { forwardRef } from "react";
 import { SiPython } from "react-icons/si";
 import { SiAmazonaws } from "react-icons/si";
 import { SiRubygems } from "react-icons/si";
 import { SiJavascript } from "react-icons/si";
 import { SiGooglecloud } from "react-icons/si";
 import { GrMysql } from "react-icons/gr";
+import { withStyles } from "@material-ui/core/styles";
+import Tooltip from "@material-ui/core/Tooltip";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+
+const LightTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: "black",
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}))(Tooltip);
+
+const IconElements = [
+  <SiPython size={35} />,
+  <SiAmazonaws size={35} />,
+  <SiRubygems size={35} />,
+  <SiJavascript size={35} />,
+  <SiGooglecloud size={35} />,
+  <GrMysql size={35} />,
+];
+
+const OnHoverElements = [
+    "Python",
+    "AWS: EC2, Lambda, Cloudformation, S3, RDS, IAM, & more",
+    "Ruby",
+    "JavaScript: Vanilla, React, and Node",
+    "GCP: Cloud Run",
+    "MySQL"
+]
+const FormattedIconElements = [];
+
+IconElements.forEach(function (icon, i) {
+  FormattedIconElements.push(
+    <Tooltip title={OnHoverElements[i]} aria-label="add">
+      <Fab>{icon}</Fab>
+    </Tooltip>
+  );
+});
+
+// TODO: use style={{flexBasis: "100%"}} styling on div, wrap every 3 elements in it
 
 const AboutMeIcons = () => {
   return (
-    <div style={{ width: "50%" }}>
+    <div style={{ width: "40%" }}>
       <h1 style={{ textAlign: "center" }}>Skills</h1>
       <div
         style={{
@@ -17,13 +60,7 @@ const AboutMeIcons = () => {
           flexWrap: "wrap",
         }}
       >
-        <SiPython size={40} />
-        <SiAmazonaws size={40} />
-        <SiRubygems size={40} />
-
-        <SiJavascript size={40} />
-        <SiGooglecloud size={40} />
-        <GrMysql size={40} />
+        {FormattedIconElements}
       </div>
     </div>
   );
