a7979bc2c7387b55d0537065c3a1f0a5a14d3b0e
Dan Murphy <danielmurph8@gmail.com>
2021-01-04 14:45:34 -0500

    remove text underneath, going to add tooltip instead

--- src/pages/contents/AboutMeIcons.js
+++ src/pages/contents/AboutMeIcons.js
@@ -1,42 +1,32 @@
-import {SiPython} from "react-icons/si";
-import {SiAmazonaws} from "react-icons/si"
-import {SiRubygems} from "react-icons/si"
-import {SiJavascript} from "react-icons/si"
-import {SiGooglecloud} from "react-icons/si"
-import {GrMysql} from "react-icons/gr"
+import { SiPython } from "react-icons/si";
+import { SiAmazonaws } from "react-icons/si";
+import { SiRubygems } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
+import { SiGooglecloud } from "react-icons/si";
+import { GrMysql } from "react-icons/gr";
 
 const AboutMeIcons = () => {
-    return (
-        <div style={{width: "50%"}}>
-            <h1 style={{textAlign: "center"}}>Skills</h1>
-            <div style={{display: "flex", justifyContent: "space-between",  margin: "1em 0", flexWrap: "wrap"}}>
-                <div>
-                    <SiPython size={40}/>
-                    <p>Python</p>
-                </div>
-                <div>
-                    <SiAmazonaws size={40}/>
-                    <p>AWS</p>
-                </div>
-                <div>
-                    <SiRubygems size={40}/>
-                    <p>Ruby</p>
-                </div>
-                <div>
-                    <SiJavascript size={40}/>
-                    <p>JS</p>
-                </div>
-                <div>
-                    <SiGooglecloud size={40}/>
-                    <p>GCP</p>
-                </div>
-                <div>
-                    <GrMysql size={40}/>
-                    <p>MySQL</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div style={{ width: "50%" }}>
+      <h1 style={{ textAlign: "center" }}>Skills</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          margin: "1em 0",
+          flexWrap: "wrap",
+        }}
+      >
+        <SiPython size={40} />
+        <SiAmazonaws size={40} />
+        <SiRubygems size={40} />
+
+        <SiJavascript size={40} />
+        <SiGooglecloud size={40} />
+        <GrMysql size={40} />
+      </div>
+    </div>
+  );
+};
 
 export default AboutMeIcons;
