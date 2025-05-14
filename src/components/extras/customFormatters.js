8364959ea04d13b06a0371d09ea88c21a462be7b
Daniel Murphy <danielmurph8@gmail.com>
2025-05-13 20:50:43 -0400

    start clean

--- src/components/extras/customFormatters.js
+++
@@ -1,5 +0,0 @@
-const formatOf = (descriptor) => {
-  return `<span style='color: #748592;text-decoration: underline; text-decoration-color: #C8C8C8; font-weight: bold;'>${descriptor}</span>`;
-};
-
-export { formatOf };
